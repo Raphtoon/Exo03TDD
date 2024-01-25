eca3661d5d9a9f2cdec5f0717798b2c6cb974c63
Raphaël Oliazadeh <Oliazadeh.raphael@gmail.com>
2024-01-25 15:52:36 +0100

    RED When_Search_EqualOrMoreThan2_ReturnCityStartBy

--- SearchTests/Search.Tests.cs
+++ SearchTests/Search.Tests.cs
@@ -2,6 +2,11 @@ using Exo03TDD.Bibliotheque;
 
 namespace SearchTests
 {
+    //   Par exemple, pour le texte de recherche "Va", la fonction doit renvoyer Valence et Vancouver 
+    //3. La fonctionnalité de recherche doit être insensible à la casse
+    //4. La fonctionnalité de recherche devrait également fonctionner lorsque le texte de recherche n'est qu'une partie d'un nom de ville
+    //   Par exemple "ape" devrait renvoyer la ville "Budapest"
+    //5. Si le texte de recherche est un « * » (astérisque), il doit renvoyer tous les noms de ville.
     [TestFixture]
     public class Tests
     {
@@ -16,6 +21,17 @@ namespace SearchTests
             //searchCity.SearchCityByName("a");
 
             Assert.Throws<NotFoundException>(() => searchCity.SearchCityByName("a"));
+        }
+
+        [Test]
+        //    2. Si le texte de recherche est égal ou supérieur à 2 caractères, il doit renvoyer tous les noms de ville commençant par le texte de recherche exact.
+        public void When_Search_EqualOrMoreThan2_ReturnCityStartBy()
+        {
+            SearchCity searchCity = new SearchCity();
+            //Act 
+            var result = searchCity.SearchCityByName("a");
+            List<String> expected = new List<String>() { "Valence", "Vancouver" };
+            CollectionAssert.AreEqual(expected, result);
 
         }
     }
